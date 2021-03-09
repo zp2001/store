6b34c5a406998eee7fa40e239e7880a09975603b
zp2001 <80318762+zp2001@users.noreply.github.com>
2021-03-09 17:55:50 +0800

    Add files via upload

---
+++ yifu.py
@@ -0,0 +1,25 @@
+bian1=1
+yifuming1="衬衫"
+jiage1=20
+pinzhi1="棉"
+leixing1="上衣"
+xiaoliang1="好"
+
+bian2=2
+yifuming2="牛仔裤"
+jiage2=20
+pinzhi2="棉"
+leixing2="裤子"
+xiaoliang2="好"
+
+print("衣服商城系统")
+print("编号  衣服名  价格  品质  类型  销量")
+print(bian1,   yifuming1,  jiage1,  pinzhi1,  leixing1,  xiaoliang1)
+print(bian2,   yifuming2,   jiage2,  pinzhi2, leixing2,   xiaoliang2)
+
+
+
+
+
+
+
